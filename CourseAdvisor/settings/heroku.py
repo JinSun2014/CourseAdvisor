4626be934b5b9f7ecf555542d4ccf278d6b557b8
Jin Sun <jinsun2014@u.northwestern.edu>
2014-10-18 15:29:15 -0500

    Add heroku get cache

--- CourseAdvisor/settings/heroku.py
+++ CourseAdvisor/settings/heroku.py
@@ -9,11 +9,29 @@ LOCAL_APPS += ('debug_toolbar', )
 
 DATABASES['default'] = dj_database_url.config()
 
-CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+
+def get_cache():
+    import os
+    try:
+        os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
+        os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+        os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+        return {
+                'default': {
+                    'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+                    'TIMEOUT': 500,
+                    'BINARY': True,
+                    'OPTIONS': {'tcp_nodelay': True}
+                    }
+                }
+    except:
+        return {
+            'default': {
+                'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
             }
         }
+
+CACHES = get_cache()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
