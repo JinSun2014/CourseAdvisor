745a9f20617af2ea3bcaf1b34898816e9ccdbd7e
Jin Sun <jinsun2014@u.northwestern.edu>
2014-10-18 15:40:47 -0500

    Set default database

--- CourseAdvisor/settings/heroku.py
+++ CourseAdvisor/settings/heroku.py
@@ -7,31 +7,8 @@ DEBUG = False
 
 LOCAL_APPS += ('debug_toolbar', )
 
-DATABASES['default'] = dj_database_url.config()
-
-
-def get_cache():
-    import os
-    try:
-        os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
-        os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
-        os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
-        return {
-                'default': {
-                    'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-                    'TIMEOUT': 500,
-                    'BINARY': True,
-                    'OPTIONS': {'tcp_nodelay': True}
-                    }
-                }
-    except:
-        return {
-            'default': {
-                'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-            }
-        }
-
-CACHES = get_cache()
+# DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
