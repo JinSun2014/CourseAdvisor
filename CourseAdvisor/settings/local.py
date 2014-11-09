2b8280f5a074e23af3365c283fdc93b91a2cb428
Jin Sun <jinsun2014@u.northwestern.edu>
2014-11-09 13:01:01 -0600

    Add local and heroku cache

--- CourseAdvisor/settings/local.py
+++ CourseAdvisor/settings/local.py
@@ -6,22 +6,22 @@ from os.path import join, normpath
 
 from CourseAdvisor.settings import *
 
-########## DEBUG CONFIGURATION
+# ######### DEBUG CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
 TEMPLATE_DEBUG = DEBUG
-########## END DEBUG CONFIGURATION
+# ######### END DEBUG CONFIGURATION
 
 
-########## EMAIL CONFIGURATION
+# ######### EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-########## END EMAIL CONFIGURATION
+# ######### END EMAIL CONFIGURATION
 
 
-########## DATABASE CONFIGURATION
+# ######### DATABASE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
@@ -33,20 +33,36 @@ DATABASES = {
         'PORT': '',
     }
 }
-########## END DATABASE CONFIGURATION
+# ######### END DATABASE CONFIGURATION
 
 
-########## CACHE CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    }
-}
-########## END CACHE CONFIGURATION
+# ######### CACHE CONFIGURATION
+def get_cache():
+    import os
+    try:
+        os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
+        os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+        os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+        return {
+                'default': {
+                    'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+                    'TIMEOUT': 500,
+                    'BINARY': True,
+                    'OPTIONS': {'tcp_nodelay': True}
+                    }
+                }
+    except:
+        return {
+              'default': {
+                  'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+                  }
+              }
+
+CACHES = get_cache()
+# ######### END CACHE CONFIGURATION
 
 
-########## TOOLBAR CONFIGURATION
+# ######### TOOLBAR CONFIGURATION
 # See: http://django-debug-toolbar.readthedocs.org/en/latest/installation.html#explicit-setup
 INSTALLED_APPS += (
     'debug_toolbar',
@@ -60,4 +76,4 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('127.0.0.1',)
-########## END TOOLBAR CONFIGURATION
+# ######### END TOOLBAR CONFIGURATION
