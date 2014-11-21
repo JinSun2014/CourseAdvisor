f15ea2fc049c1bee5b69a814f1284287c9830d1d
Jin Sun <jinsun2014@u.northwestern.edu>
2014-11-21 06:02:12 +0000

    Add question store model

--- CourseAdvisor/App/core/admin.py
+++ CourseAdvisor/App/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ('question', 'occurrences')
+    list_display = ('question', 'occurrences')
+
+admin.site.register(Question, QuestionAdmin)
