4decb1768d33f52de96d0f034456845fd35cc241
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-12-01 01:35:04 -0600

    fixed feedback button text and markup, corrected result items to exclude reasoning prompt text

--- static/js/script.js
+++ static/js/script.js
@@ -145,7 +145,7 @@ $(document).ready(function() {
 
 		            	// Add class title to list element
 		            	var classTitle = parseTitle(response.question.evidencelist[i].title);
-		            	element += (classTitle + '</h3></div><div class="result-right"><span>Related text </span><i class="fa fa-angle-down"></i></div><div class="result-expand closed"><h4>What<br><span>Watson</span><br>Found</h4><ul class="relevant-text">');
+		            	element += (classTitle + '</h3></div><div class="result-right"><span><i class="fa fa-angle-down"></i></span></div><div class="result-expand closed"><h4>What<br><span>Watson</span><br>Found</h4><ul class="relevant-text">');
 
 		            	// Add reasoning to list element
 		            	var reasoning = parseReasoning(response.question.evidencelist[i].text, qWords);
